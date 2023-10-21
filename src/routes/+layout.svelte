abc27cb2698e2798c99f3748e20632f31c7b469e
razscer0@gmail.com <razscer0@gmail.com>
2023-10-21 18:14:40 +0700

    demo something to project owner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,7 @@
 	import '../app.css';
 </script>
 
+<nav class="bg-slate-500 w-full">
+	<span>x</span>
+</nav>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let hasLogin = true;
+</script>
+
+<div class="container">
+	{#if hasLogin}
+		<h1 class="text-red-500">Welcome to SvelteKit</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	{:else}
+		<h1>Welcome to SvelteKit</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	{/if}
+</div>
